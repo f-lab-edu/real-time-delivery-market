306aae0011e5e790a5288d58af6b6652fb77e474
Hyeontaek Oh <tax941116@gmail.com>
2020-07-19 02:12:38 +0900

    [#1] 프로젝트 생성

---
+++ src/main/java/com/ht/project/realtimedeliverymarket/RealTimeDeliveryMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ht.project.realtimedeliverymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDeliveryMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeDeliveryMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ht/project/realtimedeliverymarket/RealTimeDeliveryMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ht.project.realtimedeliverymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeDeliveryMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
