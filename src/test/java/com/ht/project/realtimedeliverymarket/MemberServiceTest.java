032da5af7e290d9dce1a95e995d4fb9d994c5403
Hyeontaek Oh <tax941116@gmail.com>
2020-08-22 21:22:34 +0900

    [#13] 테스트 코드 변경

--- src/test/java/com/ht/project/realtimedeliverymarket/MemberServiceTest.java
+++ src/test/java/com/ht/project/realtimedeliverymarket/MemberServiceTest.java
@@ -10,9 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -61,19 +59,26 @@ public class MemberServiceTest {
 
   }
 
+  /**
+   * ReflectionTestUtils
+   * ReflectionTestUtils 객체는 단위 혹은 통합 테스트 시나리오상에서
+   * reflection 기반 유틸리티 메소드들을 모아놓은 클래스입니다.
+   *
+   * invokeMethod()
+   * 해당 메소드는 제공된 파라미터와 타깃 클래스에서 주어진 이름에 해당하는 메소드를 호출합니다.
+   */
   @Test
   @DisplayName("계정이 중복되면 예외가 발생합니다.")
-  public void verifyDuplicatedAccountExceptionThrown() throws NoSuchMethodException{
+  public void verifyDuplicatedAccountExceptionThrown() {
 
-    when(memberRepository.findByAccount(any(String.class))).thenReturn(java.util.Optional.of(new Member()));
+    when(memberRepository.findByAccount("test1234"))
+            .thenReturn(java.util.Optional.of(new Member()));
 
-    Method verifyDuplicatedAccount = memberService.getClass()
-              .getDeclaredMethod("verifyDuplicatedAccount", String.class);
-
-    verifyDuplicatedAccount.setAccessible(true);
-
-    assertThrows(InvocationTargetException.class,
-            () -> verifyDuplicatedAccount.invoke(memberService, "test1234"));
+    assertThrows(IllegalStateException.class,
+            () -> ReflectionTestUtils
+                    .invokeMethod(memberService,
+                            "verifyDuplicatedAccount",
+                            "test1234"));
 
   }
 
