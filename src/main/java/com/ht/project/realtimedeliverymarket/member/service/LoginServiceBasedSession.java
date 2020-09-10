2647616de6472eb5b855867f3bb48b573f0e08de
Hyeontaek Oh <tax941116@gmail.com>
2020-09-11 00:22:59 +0900

    [#22] 로그인 구현

--- src/main/java/com/ht/project/realtimedeliverymarket/member/service/LoginServiceBasedSession.java
+++ src/main/java/com/ht/project/realtimedeliverymarket/member/service/LoginServiceBasedSession.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.servlet.http.HttpSession;
 import java.time.Duration;
 
-@Service
+@Service("loginService")
 public class LoginServiceBasedSession implements LoginService{
 
   public static final String MEMBER = "account";
