f176738748142d82b2b88838b898240f93fa1437
Hyeontaek Oh <tax941116@gmail.com>
2020-10-03 14:59:20 +0900

    [#36] 사용자 인가 기능 구현 - nullable 메소드 -> NotNullable 메소드 변경

--- src/main/java/com/ht/project/realtimedeliverymarket/member/aop/MemberAspect.java
+++ src/main/java/com/ht/project/realtimedeliverymarket/member/aop/MemberAspect.java
@@ -14,11 +14,19 @@ import static com.ht.project.realtimedeliverymarket.member.service.SessionLoginS
 @Component
 public class MemberAspect {
 
+  /**
+   * RequestContextHolder.getRequestAttributes() : 현재 스레드에 바인딩된 RequestAttribute 객체를 반환합니다.
+   * 존재하지 않으면 null 을 반환합니다.
+   *
+   * RequestContextHolder.currentRequestAttributes() : 현재 스레드에 바인딩된 RequestAttribute 객체를 반환합니다.
+   * 존재하지 않으면 IllegalStateException 을 발생합니다.
+   * @param joinPoint
+   */
   @Before("@annotation(com.ht.project.realtimedeliverymarket.member.annotation.LoginCheck)")
   public void loginCheck(JoinPoint joinPoint) {
 
     if (((ServletRequestAttributes) RequestContextHolder
-            .getRequestAttributes())
+            .currentRequestAttributes())
             .getRequest()
             .getSession()
             .getAttribute(MEMBER_SESSION_KEY) == null) {
